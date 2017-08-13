022c5d90ba0476a75e53df659115cb9f34e04945
Robin van der Vleuten <robin@webstronauts.co>
2017-08-13 15:15:55 +0200

    Clean dist directory before building + removed compiled files from repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const $ = require('gulp-load-plugins')()
+const del = require('del')
 const gulp = require('gulp')
 
 process.on('unhandledRejection', err => {
   throw err
 })
 
-gulp.task('styles', () => (
+gulp.task('clean', () =>
+  del('dist/**') 
+)
+
+gulp.task('styles', ['clean'], () => (
   gulp.src('src/**/*.scss')
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.postcss())
